3d4c8afd0e465ca5b8f7b96234361d5a63904881
andrespdx <andg2@pdx.edu>
2025-09-23 18:27:28 -0700

    Parser handles multiple files

--- index.js
+++ index.js
@@ -15,52 +15,66 @@ const projectId = process.env.PROJECT_ID;
 const location = process.env.LOCATION;
 const processorId = process.env.PROCESSOR_ID;
 
-app.post('/parse', upload.single('file'), async (req, res) => {
+app.post('/readReceipt', upload.array('files'), async (req, res) => {
     try {
-        if (!req.file) {
-            return res.status(400).send('No file uploaded.');
+        if (!req.files || req.files.length === 0) {
+            return res.status(400).send('No files uploaded.');
         }
 
-        const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
+        const results = [];
 
-        const request = {
-            name,
-            rawDocument: {
-                content: req.file.buffer,
-                mimeType: req.file.mimetype,
-            },
-            fieldMask: [
-                "document.entities.total_amount",
-                "document.entities.net_amount",
-                "document.entities.supplier_name",
-                "document.entities.receipt_date"
-            ],
-        };
+        for (const file of req.files) {
+            const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
 
-        const [result] = await docClient.processDocument(request);
-        const { document } = result;
+            const request = {
+                name,
+                rawDocument: {
+                    content: file.buffer,
+                    mimeType: file.mimetype,
+                },
+                fieldMask: [
+                    "document.entities.total_amount",
+                    "document.entities.net_amount",
+                    "document.entities.supplier_name",
+                    "document.entities.receipt_date"
+                ],
+            };
 
-        const cleanDoc = {};
-        document.entities.forEach(entity => {
-            switch(entity.type) {
-                case 'supplier_name':
-                    cleanDoc.merchant = entity.normalizedValue?.text || entity.mentionText;
-                    break;
-                case 'total_amount':
-                    if (entity.normalizedValue?.moneyValue) {
-                        const { units, nanos } = entity.normalizedValue.moneyValue;
-                        cleanDoc.total = Number(units) + Number(nanos || 0) / 1e9;
-                    } else {
-                        cleanDoc.total = entity.mentionText;
-                    }
-                case 'receipt_date':
-                    cleanDoc.date = entity.normalizedValue?.dateValue 
-                        ? `${entity.normalizedValue.dateValue.year}-${entity.normalizedValue.dateValue.month}-${entity.normalizedValue.dateValue.day}`
-                        : entity.mentionText;
-                    break;
+            const [result] = await docClient.processDocument(request);
+            if (result.document) {
+                results.push(result.document);
             }
-        });
-        res.json(cleanDoc);
+        }
+
+        const cleanDocs = [];
+
+        for (const doc of results) {
+            if (!doc.entities) continue;
+
+            const cleanDoc = {};
+
+            doc.entities.forEach(entity => {
+                switch(entity.type) {
+                    case 'supplier_name':
+                        cleanDoc.merchant = entity.normalizedValue?.text || entity.mentionText;
+                        break;
+                    case 'total_amount':
+                        if (entity.normalizedValue?.moneyValue) {
+                            const { units, nanos } = entity.normalizedValue.moneyValue;
+                            cleanDoc.total = Number(units) + Number(nanos || 0) / 1e9;
+                        } else {
+                            cleanDoc.total = entity.mentionText;
+                        }
+                    case 'receipt_date':
+                        cleanDoc.date = entity.normalizedValue?.dateValue 
+                            ? `${entity.normalizedValue.dateValue.year}-${entity.normalizedValue.dateValue.month}-${entity.normalizedValue.dateValue.day}`
+                            : entity.mentionText;
+                        break;
+                }
+            });
+            cleanDocs.push(cleanDoc);
+        }
+        res.json(cleanDocs);
     }
     catch (error) {
         console.error('Error processing document:', error);
