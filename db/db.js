e7291bc0ac62d2b8e8d6062ac259217eda2fe23e
andrespdx <andg2@pdx.edu>
2025-11-09 18:08:45 -0800

    fix2

--- db/db.js
+++ db/db.js
@@ -1,13 +1,18 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: false
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT,
+	ssl:
+		process.env.DB_HOST &&
+		process.env.DB_HOST !== "localhost" &&
+		process.env.DB_HOST !== "127.0.0.1"
+			? { rejectUnauthorized: false }
+			: false,
 });
 
 module.exports = pool;
